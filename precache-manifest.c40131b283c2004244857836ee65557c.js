e1a56b117ebc948df685ca48df4ef7941bb51d25
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-01-27 20:39:43 +0300

    Updates

---
+++ precache-manifest.c40131b283c2004244857836ee65557c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0f673b0c62e0504eddfd25bd58fe3b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b9786d2f8bcf83457ba7",
+    "url": "/todolist/static/css/main.9cee13af.chunk.css"
+  },
+  {
+    "revision": "368080ab82e03865fa31",
+    "url": "/todolist/static/js/2.d6d3d542.chunk.js"
+  },
+  {
+    "revision": "6127279098a66fe146baae54c06b7218",
+    "url": "/todolist/static/js/2.d6d3d542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9786d2f8bcf83457ba7",
+    "url": "/todolist/static/js/main.35ce8d63.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/todolist/static/js/main.35ce8d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9559cebae45c40ec80",
+    "url": "/todolist/static/js/runtime-main.8a5a1b18.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist/precache-manifest.7a6860b5997e1806751e6442bfab6ed6.js"
+  "/todolist/precache-manifest.c40131b283c2004244857836ee65557c.js"
 );
 
 self.addEventListener('message', (event) => {
